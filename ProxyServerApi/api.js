3c375180eeb1858ba2a9a97a0fca8db4b3ff9749
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-01-07 16:37:11 +0530

    Updated ProxyServerApi

--- ProxyServerApi/api.js
+++ ProxyServerApi/api.js
@@ -1,11 +1,18 @@
+// Importing Express to create the server
 const express = require('express')
+
+// Initializing the Express application
 const app = express()
+
+// Defining the port where the server will run
 const port = 3000
 
+// Setting up the route for the root URL ('/')
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!!')
 })
 
+// Starting the server and listening on the defined port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Proxy Server API is listening at port ${port}`)
 })
