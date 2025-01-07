b9aad06fd542268f4fcfc7bce41a5a69db49f6db
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-01-07 16:53:59 +0530

    Added Puppeteer

--- ProxyServerApi/api.js
+++ ProxyServerApi/api.js
@@ -1,27 +1,39 @@
-// Importing Express to create the server
-const express = require('express')
+import puppeteer from 'puppeteer';
+import express from 'express';
+
+// Launch the browser and open a new blank page
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
 
 // Initializing the Express application
-const app = express()
+const app = express();
 
 // Defining the port where the server will run
-const port = 3000
+const port = 3000;
+
+// Middleware to log request and response time
+app.use((req, res, next) => {
+  const currentDate = new Date();
+  const startTime = currentDate.getTime();
+
+  console.log(`Request Time: ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`);
+
+  // Once the response is sent, log the response time
+  res.on('finish', () => {
+    const endTime = new Date().getTime();
+    const duration = endTime - startTime;
+    console.log(`Response Time: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} - Duration: ${duration}ms`);
+  });
+
+  next(); // Proceed to the next middleware or route handler
+});
 
 // Setting up the route for the root URL ('/')
 app.get('/', (req, res) => {
-  const currentDate = new Date();
-  var hours = currentDate.getHours();
-  var minutes = currentDate.getMinutes();
-  var seconds = currentDate.getSeconds();
-  console.log(`Request Time: ${hours}:${minutes}:${seconds}`);
-  res.send('Hello World!!')
-  hours = currentDate.getHours();
-  minutes = currentDate.getMinutes();
-  seconds = currentDate.getSeconds();
-  console.log(`Response Time: ${hours}:${minutes}:${seconds}`);
-})
+  res.send('Hello World!!');
+});
 
 // Starting the server and listening on the defined port
 app.listen(port, () => {
-    console.log(`Proxy Server API is listening at port ${port}`)
-})
+  console.log(`Proxy Server API is listening at port ${port}`);
+});
