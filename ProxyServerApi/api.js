36d9690fc4d531a4b1c530a0b932322bbe59b8cc
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-01-07 16:43:22 +0530

    Update api.js

--- ProxyServerApi/api.js
+++ ProxyServerApi/api.js
@@ -9,7 +9,16 @@ const port = 3000
 
 // Setting up the route for the root URL ('/')
 app.get('/', (req, res) => {
-    res.send('Hello World!!')
+  const currentDate = new Date();
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds();
+  console.log(`Request Time: ${hours}:${minutes}:${seconds}`);
+  res.send('Hello World!!')
+  hours = currentDate.getHours();
+  minutes = currentDate.getMinutes();
+  seconds = currentDate.getSeconds();
+  console.log(`Response Time: ${hours}:${minutes}:${seconds}`);
 })
 
 // Starting the server and listening on the defined port
