334397225ae5243f170fbca3615788d3e100a8c8
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-01-08 22:55:40 +0530

    first commit

--- ProxyServerApi/api.js
+++ ProxyServerApi/api.js
@@ -1,39 +1,59 @@
-import puppeteer from 'puppeteer';
 import express from 'express';
+import puppeteer from 'puppeteer';
+import cors from 'cors';
 
-// Launch the browser and open a new blank page
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-
-// Initializing the Express application
 const app = express();
-
-// Defining the port where the server will run
 const port = 3000;
+
+app.use(cors())
 
 // Middleware to log request and response time
 app.use((req, res, next) => {
-  const currentDate = new Date();
-  const startTime = currentDate.getTime();
+  const startTime = Date.now();
+  console.log(`Request Time: ${new Date().toISOString()}`);
 
-  console.log(`Request Time: ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`);
-
-  // Once the response is sent, log the response time
   res.on('finish', () => {
-    const endTime = new Date().getTime();
-    const duration = endTime - startTime;
-    console.log(`Response Time: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} - Duration: ${duration}ms`);
+    const duration = Date.now() - startTime;
+    console.log(`Response Time: ${new Date().toISOString()} - Duration: ${duration}ms`);
   });
 
-  next(); // Proceed to the next middleware or route handler
+  next();
 });
 
-// Setting up the route for the root URL ('/')
+// Endpoint to fetch HTML content
+app.get('/fetch', async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).send({ error: 'URL parameter is required.' });
+  }
+
+  try {
+    const decodedUrl = decodeURIComponent(url);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(decodedUrl, { waitUntil: 'networkidle2' });
+
+    const htmlContent = await page.content();
+
+    await browser.close();
+
+    res.set('Content-Type', 'text/html');
+    res.send(htmlContent);
+
+  } catch (error) {
+    console.error('Error fetching the HTML content:', error);
+    res.status(500).send({ error: 'Failed to fetch the HTML content. Check the URL and try again.' });
+  }
+});
+
+// Root route
 app.get('/', (req, res) => {
   res.send('Hello World!!');
 });
 
-// Starting the server and listening on the defined port
+// Start the server
 app.listen(port, () => {
   console.log(`Proxy Server API is listening at port ${port}`);
 });
