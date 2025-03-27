c3229ad6a160401d64b6628523bb6a20c0b19c4d
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-03-28 00:43:01 +0530

    CORS trial 😢

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,10 +53,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    "corsheaders",
     'products_api',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -67,6 +69,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "backend.urls"
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
