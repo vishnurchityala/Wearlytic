8ca9807b80bdfa5aa5884105482f72eaf1614f3b
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-03-27 22:30:03 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,4 +123,11 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+ALLOWED_HOSTS = [
+    "wearlytic-backend.vercel.app",
+    "127.0.0.1",
+    "localhost",
+]
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
