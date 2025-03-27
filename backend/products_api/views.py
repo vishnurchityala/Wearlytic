04bbad34fcd6ba6d837f9dec85ba0477daca0294
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-03-27 23:23:51 +0530

    Update views.py

--- backend/products_api/views.py
+++ backend/products_api/views.py
@@ -20,6 +20,7 @@ class ProductPagination(PageNumberPagination):
 @api_view(['GET'])
 def get_products(request):
     try:
+        # Hellloooo
         products_ref = db.collection('products')
         all_products = [doc.to_dict() for doc in products_ref.stream()]
 
