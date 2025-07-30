fa3b331645d83f5a45a4710f0e59218bb2392ec6
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-07-30 14:56:36 +0530

    Makfile Config

--- scraping_agent/main.py
+++ scraping_agent/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from api.routes import listing, product, status
 
-app = FastAPI(title="ScraperKit API")
+app = FastAPI(title="ScrapingAgent API")
 
 app.include_router(listing.router)
 app.include_router(product.router)
