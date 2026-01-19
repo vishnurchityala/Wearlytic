d171d2d2f0a317ec8935618f55d3b3dfc6860bc3
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2026-01-19 14:45:01 +0530

    Fixes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path("", lambda request: HttpResponse("OK")),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
